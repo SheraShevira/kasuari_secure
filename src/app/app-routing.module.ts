76c0d26298462f72b04ac061fbaf0e308efc0fde
Kuuramur <jibrilarief.dev@gmail.com>
2024-02-27 18:02:15 +0700

    push-testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'kebakaran-page',
+    loadChildren: () => import('./kebakaran-page/kebakaran-page.module').then( m => m.KebakaranPagePageModule)
   },
+
 ];
 
 @NgModule({
